ea16aeee60bf7408d85700f38182b1b8fd68740a
Nioxed <admin@nioxed.com>
2018-08-18 22:07:31 +0200

    add more examples

---
+++ examples/npmscript.js
@@ -0,0 +1,39 @@
+var options = {
+    repo        : "Nioxed/github-update",
+    branch      : "master",
+    packageFile : "package.json",
+    localPath   : "./repo-npmscript",
+    runScript   : "exampleScript",
+    debug       : true
+        
+}
+    
+var updater = require('../')(options);
+
+
+updater.check( ( error, upToDate ) => {
+    if(error){ throw error; }
+
+    if(upToDate){
+		
+		console.log('App is up to date');
+        StartApp();
+		
+    }else{
+
+        updater.update( (success, error) => {
+			if(!success){ throw error; };
+				
+			console.log('Updated App')
+			StartApp();
+        })
+
+    }
+
+})
+
+function StartApp(){
+    console.log("App Started");
+    process.exit(0);
+}
+
